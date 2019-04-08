749ac5ac8fd0853ae22a9cd471138114d8081282
muerwre <gotham48@gmail.com>
2019-04-08 14:51:34 +0700

    fixed exports?

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import * as searchReducer from '$redux/search/reducer';
-import * as searchSagas from '$redux/search/sagas';
-import { SEARCH_PATHS } from "$constants/paths";
-import { SEARCH_ACTIONS } from "$redux/search/constants";
-import * as searchActions from '$redux/search/actions';
-
-export {
-  searchActions,
-  searchReducer,
-  searchSagas,
-  SEARCH_ACTIONS,
-  SEARCH_PATHS,
-};
+export { default as searchReducer, searchPersistConfig } from '$redux/search/reducer';
+export { default as searchSaga } from '$redux/search/sagas';
+export { SEARCH_PATHS } from "$constants/paths";
+export { SEARCH_ACTIONS } from "$redux/search/constants";
+export * from '$redux/search/actions';
