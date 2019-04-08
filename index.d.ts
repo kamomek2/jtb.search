87b963fbac989978f9df23d13daf6e4254782f8e
muerwre <gotham48@gmail.com>
2019-04-08 17:40:38 +0700

    added more typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 import { Reducer } from "redux";
 import { PersistConfig } from "redux-persist/es/types";
 import * as React from "react";
+import * as searchActions from './src/redux/search/actions';
 
 declare const searchReducer: Reducer;
 declare const searchPersistConfig: PersistConfig;
@@ -8,5 +9,10 @@ declare const searchSaga;
 declare const SEARCH_PATHS;
 declare const SEARCH_ACTIONS;
 declare class SearchScreen extends React.PureComponent<{}, {}> {}
+
+
+declare const searchSetSearchString: typeof searchActions.searchSetSearchString;
+declare const searchPerformSearch: typeof searchActions.searchPerformSearch;
+declare const searchDispatchSearchQuery: typeof searchActions.searchDispatchSearchQuery;
 
 declare module 'jtb.search' {}
