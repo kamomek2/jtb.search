f10943c778abf65de175f01a7ef1b669bf3d9e61
muerwre <gotham48@gmail.com>
2019-04-08 15:55:57 +0700

    exported search screen

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as searchSaga } from '$redux/search/sagas';
 export { SEARCH_PATHS } from "$constants/paths";
 export { SEARCH_ACTIONS } from "$redux/search/constants";
 export * from '$redux/search/actions';
+export { default as SearchScreen } from '$components/SearchScreen';
